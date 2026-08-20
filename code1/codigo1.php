61a2fb77e89ad0caad8cc2dab9e68154a4a527e0
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-08-20 12:17:16 -0300

    up - tirei a senha

--- code1/codigo1.php
+++ code1/codigo1.php
@@ -2,10 +2,9 @@
 
 $host = "localhost";
 $user = "root";
-$passaword = "root";
 $database = "caca_aos_erros";
 
-$conexao = new mysqli($host, $user, $password, $database);
+$conexao = new mysqli($host, $user, $database);
 
 if ($conexao->connect_error) {
     die ("Erro na conexão: " . $conexao->connect_error);
