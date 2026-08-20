31d2273e6b4223d23c2bf18b632abde10005888e
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-08-20 12:15:46 -0300

    up - adicionei um ; que estava faltando

--- code1/codigo1.php
+++ code1/codigo1.php
@@ -8,7 +8,7 @@ $database = "caca_aos_erros";
 $conexao = new mysqli($host, $user, $password, $database);
 
 if ($conexao->connect_error) {
-    die ("Erro na conexão: " . $conexao->connect_error)
+    die ("Erro na conexão: " . $conexao->connect_error);
 }
 
 if (isset($_POST['cadastrar'])){
