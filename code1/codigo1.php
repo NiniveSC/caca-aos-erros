d93d34a42c787cfbe30f4d1966b472c55e1c6ce9
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-08-20 12:00:38 -0300

    up - arrumei uma palavra que tinha escrito errado

--- code1/codigo1.php
+++ code1/codigo1.php
@@ -7,7 +7,7 @@ $database = "caca_aos_erros"
 
 $conexao = new mysqli($host, $user, $password, $database)
 
-if ($conexao->conexaoect_error) {
+if ($conexao->connect_error) {
     die ("Erro na conexão: " . $conexao->connect_error)
 }
 
